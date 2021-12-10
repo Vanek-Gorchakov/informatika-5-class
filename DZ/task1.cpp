9549879cc714dbecb344586718d67e1e473ffbed
Vanek-Gorchakov <79834049+Vanek-Gorchakov@users.noreply.github.com>
2021-12-10 19:07:05 +0300

    Create task1.cpp

---
+++ DZ/task1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+template < typename T, typename F >
+void quick_sort(T* arr, int left, int right, F compare)
+{
+	if (right - left < 1) { return };
+
+	int k = left;
+
+	for (auto i = left; i <= right; ++i)
+	{
+		if compare(arr[i], arr[right])
+		{
+			std::swap(arr[i], arr[k++]);
+		}
+	}
+	std::swap(arr[k], arr[right]);
+	quick_sort(arr, left, k - 1, compare);
+	quick_sort(arr, k + 1, right, compare);
+}
+
+
+template< typename T, std::size_t N, typename F > // статический массив
+void sort(T (&arr) [N], F compare)
+{
+	quick_sort(arr, 0, N - 1, F compare);
+}
+
+template < typename T, typename F  > // динамический массив
+void sort(T* arr, std::size_t size, F compare)
+{
+	quick_sort(arr, 0, size - 1, F compare);
+}
